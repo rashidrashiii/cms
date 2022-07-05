292b9ef38fed8bace86f5bc09bd6d18847b3f650
Muhammed Rashid V <87640868+rashidrashiii@users.noreply.github.com>
2022-07-05 16:16:04 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -2,7 +2,11 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('
+      'PqT1DFhABW2knMmkaM8y8g==',
+      'EolyA2Sjk0wcU/7xyc3lrA==',
+      'jvhQLJNH8H3gol+aQRyN6A==',
+      'pkmwSSyU1Km8AZpZklh97g=='),
   },
   admin: {
     watchIgnoreFiles: ["**/private/**"],
